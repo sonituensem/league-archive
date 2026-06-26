c8e9dcc69987cc484cfa9c125edc8829db4f0236
Yatsenko Dmytro <sonituensem@gmail.com>
2026-06-26 13:20:07 +0300

    Add database models and configure admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from catalog.models import Champion, Region, Role, Skin, User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Champion)
+class ChampionAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "title",
+        "region",
+        "difficulty",
+        "release_year",
+    )
+    search_fields = ("name", "title")
+    list_filter = ("region", "roles")
+
+
+@admin.register(Skin)
+class SkinAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "champion",
+        "release_year",
+    )
+    search_fields = ("name",)
--- catalog/models.py
+++ catalog/models.py
@@ -4,3 +4,55 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
+class Role(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
+class Champion(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+
+    difficulty = models.PositiveIntegerField()
+
+    release_year = models.PositiveIntegerField()
+
+    region = models.ForeignKey(
+        Region,
+        on_delete=models.CASCADE,
+        related_name="champions",
+    )
+
+    roles = models.ManyToManyField(
+        Role,
+        related_name="champions",
+    )
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Skin(models.Model):
+    name = models.CharField(max_length=150)
+
+    release_year = models.PositiveIntegerField()
+
+    champion = models.ForeignKey(
+        Champion,
+        on_delete=models.CASCADE,
+        related_name="skins",
+    )
+
+    def __str__(self) -> str:
+        return self.name
